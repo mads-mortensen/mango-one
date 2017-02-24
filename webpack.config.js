c447ee42cb71d1bff6ffe2d6d9004233e30616d4
Mads Mortensen <madsxp+github@gmail.com>
2017-02-24 13:48:01 +0100

    Add ESLint and eslint-loader. Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,11 @@ module.exports = {
     },
     module: {
 	    rules: [{
+            enforce: "pre",
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "eslint-loader",
+        }, {
             test: /\.js$/,
             exclude: /(node_modules|bower_components)/,
             loader: 'babel-loader?presets[]=es2015'
